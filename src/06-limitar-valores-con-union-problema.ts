e8d9705c99e6527d96620ec1a2a15fc32112166c
Matias Baldanza <matiasbaldanza@gmail.com>
2025-01-11 13:06:43 -0300

    problema 6 - limitar valores con union types

---
+++ src/06-limitar-valores-con-union-problema.ts
@@ -0,0 +1,32 @@
+// Missing properties in type / ts(2379)
+
+interface User {
+  id: number
+  firstName: string
+  lastName: string
+  isAdmin: boolean
+  role: string   // Cómo limitamos los valores posibles para 'role'?
+}
+
+const defaultUser: User = {
+  id: 1,
+  firstName: "Matias",
+  lastName: "Baldanza",
+  isAdmin: true,
+  // @ts-expect-error
+  role: "Rol que no existe"
+}
+
+export function userPermissions(user: User) {
+  switch (user.role) {
+    case "admin":
+      return "Admin"
+    case "user":
+      return "User"
+    default:
+      return "Unknown"
+  }
+}
+
+const permissions = userPermissions(defaultUser)
+console.log(permissions)
---
+++ src/06-limitar-valores-con-union-solucion.ts
@@ -0,0 +1,28 @@
+// Missing properties in type / ts(2379)
+
+interface User {
+  id: number
+  firstName: string
+  lastName: string
+  isAdmin: boolean
+  role: "admin" | "user" | "guest"
+}
+
+const defaultUser: User = {
+  id: 1,
+  firstName: "Matias",
+  lastName: "Baldanza",
+  isAdmin: true,
+  // @ts-expect-error
+  role: "Rol que no existe"
+}
+
+// En los union types podemos agregar cualquier tipo de valor.
+
+let role: "admin" | "user" | { wow: boolean }
+
+role = "admin"
+role = "user"
+role = { wow: true }
+role = { wow: 23 }
+
