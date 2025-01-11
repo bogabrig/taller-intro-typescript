c71d7c8d1a798b7b69bd31c2034221eaf9e367f7
Matias Baldanza <matiasbaldanza@gmail.com>
2025-01-11 13:11:38 -0300

    problema 7 - arrays

---
+++ src/07-arrays-solucion-2.ts
@@ -0,0 +1,31 @@
+// Missing properties in type / ts(2379)
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  role: "admin" | "user" | "super-admin";
+  posts: Array<Post>;  // Array de posts con sintaxis de generics
+}
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+export const defaultUser: User = {
+  id: 1,
+  firstName: "Matias",
+  lastName: "Baldanza",
+  role: "admin",
+  posts: [
+    {
+      id: 1,
+      title: "Aprender TypeScript es fácil",
+    },
+    {
+      id: 2,
+      title: "Conceptos avanzados de TypeScript",
+    },
+  ],
+}
